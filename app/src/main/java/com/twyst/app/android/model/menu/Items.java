d967883aeae660cb3b5e45aed1bd0d501a075d31
Vipul <vipul.sharma2008@gmail.com>
2015-12-18 17:46:49 +0530

    SKUs

--- app/src/main/java/com/twyst/app/android/model/menu/Items.java
+++ app/src/main/java/com/twyst/app/android/model/menu/Items.java
@@ -178,15 +178,71 @@ public class Items implements Serializable {
 
     @Override
     public boolean equals(Object obj) {
+        Items newItem = (Items) this;
         if (obj instanceof Items) {
-            Items item = (Items) obj;
-            if (item != null &&
-                    this.id.equals(item.id) &&
-                    this.getOptionsList().get(0).getId().equals(item.getOptionsList().get(0).getId())
-                    ) {
+            Items oldItem = (Items) obj;
+            if (oldItem != null &&
+                    newItem.id.equals(oldItem.id) &&
+                    isOptionListEqual(newItem, oldItem)) {
                 return true;
             }
         }
         return false;
+    }
+
+    private boolean isOptionListEqual(Items newItem, Items oldItem) {
+        if (newItem.getOptionsList().size() > 0) {
+            Options newOption = newItem.getOptionsList().get(0);
+            Options oldOption = oldItem.getOptionsList().get(0);
+            return (newOption.getId().equals(oldOption.getId()) &&
+                    isSubOptionListEqual(newOption, oldOption) &&
+                    isAddOnListEqual(newOption, oldOption));
+        }
+        return true;
+    }
+
+    private boolean isSubOptionListEqual(Options newOption, Options oldOption) {
+        if (newOption.getSubOptionsList().size() > 0) {
+            for (int i = 0; i < newOption.getSubOptionsList().size(); i++) {
+                if (!newOption.getSubOptionsList().get(i).getSubOptionSetList().get(0).getId().equals(
+                        oldOption.getSubOptionsList().get(i).getSubOptionSetList().get(0).getId())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isAddOnListEqual(Options newOption, Options oldOption) {
+        if (newOption.getAddonsList().size() > 0) {
+            for (int i = 0; i < newOption.getAddonsList().size(); i++) {
+                if (!isAddOnSetListEqual(newOption.getAddonsList().get(i).getAddonSetList(), oldOption.getAddonsList().get(i).getAddonSetList())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isAddOnSetListEqual(ArrayList<AddonSet> newAddonSetList, ArrayList<AddonSet> oldAddonSetList) {
+        if (newAddonSetList.size() != oldAddonSetList.size()) {
+            return false;
+        } else {
+            for (int i = 0; i < newAddonSetList.size(); i++) {
+                AddonSet newAddonSet = newAddonSetList.get(i);
+                boolean newAddonSetFound = false;
+                for (int j = 0; j < oldAddonSetList.size(); j++) {
+                    AddonSet oldAddonSet = oldAddonSetList.get(j);
+                    if (newAddonSet.getId().equals(oldAddonSet.getId())) {
+                        newAddonSetFound = true;
+                        break;
+                    }
+                }//inner loop
+                if (!newAddonSetFound) {
+                    return false;
+                }
+            } //outer loop
+        }
+        return true;
     }
 }
