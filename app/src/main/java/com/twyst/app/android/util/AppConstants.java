f7926257ceffaa8026d02856321499450da693e6
Vipul <vipul.sharma2008@gmail.com>
2016-01-11 20:01:34 +0530

    Address Details constants

--- app/src/main/java/com/twyst/app/android/util/AppConstants.java
+++ app/src/main/java/com/twyst/app/android/util/AppConstants.java
@@ -160,18 +160,16 @@ public class AppConstants {
 
     public static final String MAP_TO_BE_SHOWN = "MAP_TO_BE_SHOWN";
     public static final String DATA_TO_BE_SHOWN = "DATA_TO_BE_SHOWN";
+    public static final String ADDRESS_FOUND = "ADDRESS_FOUND";
 
     public static final int SUCCESS_RESULT = 0;
-
     public static final int FAILURE_RESULT = 1;
 
     public static final String PACKAGE_NAME =
             "com.google.android.gms.location.sample.locationaddress";
-
     public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
-
+    public static final String ADDRESS_DATA_KEY = PACKAGE_NAME + ".ADDRESS_DATA_KEY";
     public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
-
     public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
 
 }
