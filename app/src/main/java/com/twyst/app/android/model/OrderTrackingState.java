2990532216077515697788509fda50c805a7877c
Vipul <vipul.sharma2008@gmail.com>
2016-01-27 14:53:10 +0530

    OrderTrackingState

---
+++ app/src/main/java/com/twyst/app/android/model/OrderTrackingState.java
@@ -0,0 +1,54 @@
+package com.twyst.app.android.model;
+
+/**
+ * Created by Vipul Sharma on 1/27/2016.
+ */
+public class OrderTrackingState {
+    //Order States
+    public static final int ORDER_PLACED = 1;
+    public static final int ORDER_ACCEPTED = 2;
+    public static final int ORDER_DISPATCHED = 3;
+    public static final int ORDER_ASSUMED_DELIVERY = 4;
+    public static final int ORDER_NOT_DELIVERED = 5;
+
+    // AMPM time
+    public static final String ORDER_TIME_AM = "AM";
+    public static final String ORDER_TIME_PM = "PM";
+
+    private String time;
+    private String ampm;
+    private String message;
+    private String orderState;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAmpm() {
+        return ampm;
+    }
+
+    public void setAmpm(String ampm) {
+        this.ampm = ampm;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getOrderState() {
+        return orderState;
+    }
+
+    public void setOrderState(String orderState) {
+        this.orderState = orderState;
+    }
+}
