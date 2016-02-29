73dfce28472fddb4ea931f9240e908f1f8b6dd66
Vipul <vipul.sharma2008@gmail.com>
2016-02-29 12:41:11 +0530

    contact picked validation

--- app/src/main/java/com/twyst/app/android/activities/RechargeActivity.java
+++ app/src/main/java/com/twyst/app/android/activities/RechargeActivity.java
@@ -37,8 +37,9 @@ public class RechargeActivity extends BaseActionActivity {
     }
 
     private void openContacts() {
-        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(intent, PICK_CONTACT);
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT);
     }
 
     @Override
@@ -46,66 +47,53 @@ public class RechargeActivity extends BaseActionActivity {
         super.onActivityResult(reqCode, resultCode, data);
         switch (reqCode) {
             case (PICK_CONTACT):
+                // Make sure the request was successful
+                if (resultCode == RESULT_OK) {
+                    // Get the URI that points to the selected contact
+                    Uri contactUri = data.getData();
+                    // We only need the NUMBER column, because there will be only one row in the result
+                    String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
 
-                try {
-                    if (resultCode == Activity.RESULT_OK) {
-                        Uri contactData = data.getData();
-                        Cursor cur = managedQuery(contactData, null, null, null, null);
-                        ContentResolver contect_resolver = getContentResolver();
+                    // Perform the query on the contact to get the NUMBER column
+                    // We don't need a selection or sort order (there's only one result for the given URI)
+                    // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                    // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                    // Consider using CursorLoader to perform the query.
+                    Cursor cursor = getContentResolver()
+                            .query(contactUri, projection, null, null, null);
+                    cursor.moveToFirst();
 
-                        if (cur.moveToFirst()) {
-                            String id = cur.getString(cur.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
-                            String name = "";
-                            String no = "";
-
-                            Cursor phoneCur = contect_resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
-
-                            if (phoneCur.moveToFirst()) {
-                                name = phoneCur.getString(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                                no = phoneCur.getString(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            }
-
-                            updateContact(no);
-
-                            id = null;
-                            name = null;
-                            no = null;
-                            phoneCur = null;
-                        }
-                        contect_resolver = null;
-                        cur = null;
-                        //                      populateContacts();
-                    }
-                } catch (IllegalArgumentException e) {
-                    e.printStackTrace();
-                    Log.e("IllegalArgException", e.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Log.e("Error :: ", e.toString());
+                    // Retrieve the phone number from the NUMBER column
+                    int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    String number = cursor.getString(column);
+                    updateContact(number);
                 }
-
-
                 break;
         }
     }
 
     private void updateContact(String number) {
-        if (!TextUtils.isEmpty(number)) {
+        String formattedNumber = getFormattedNumber(number);
+        if (formattedNumber.length() == 10) {
             EditText etNumber = (EditText) findViewById(R.id.et_number);
-            etNumber.setText(number);
+            etNumber.setText(formattedNumber);
         } else {
             Toast.makeText(RechargeActivity.this, "Could not read phone number!", Toast.LENGTH_SHORT).show();
         }
     }
 
-//    private String getFormattedNumber(String number) {
-//        if (number.length() > 10) {
-//
-//        } else if () {
-//
-//        }
-//        return "";
-//    }
+    private String getFormattedNumber(String number) {
+        if (!TextUtils.isEmpty(number)) {
+            number = number.replaceAll("[^0-9]", "");
+            if (number.length() == 10) {
+                return number;
+            } else if (number.length() == 11 && number.substring(0, 1).equals("0")) {
+                return number.substring(1);
+            } else if (number.length() == 12 && number.substring(0, 2).equals("91")) {
+                return number.substring(2);
+            }
+        }
+        return "";
+    }
 
 }
