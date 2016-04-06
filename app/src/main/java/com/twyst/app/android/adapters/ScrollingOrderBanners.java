22d2c1c515ae8d2f7e8596047a818cf870f86f09
Vipul <vipul.sharma2008@gmail.com>
2016-04-06 20:42:00 +0530

    Webview load url

--- app/src/main/java/com/twyst/app/android/adapters/ScrollingOrderBanners.java
+++ app/src/main/java/com/twyst/app/android/adapters/ScrollingOrderBanners.java
@@ -1,15 +1,22 @@
 package com.twyst.app.android.adapters;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,8 +29,11 @@ import com.twyst.app.android.fragments.DiscoverOutletFragment;
 import com.twyst.app.android.model.Offer;
 import com.twyst.app.android.model.banners.OrderBanner;
 import com.twyst.app.android.util.AppConstants;
+import com.twyst.app.android.util.TwystProgressHUD;
 
 import java.util.ArrayList;
+
+import fr.castorflex.android.circularprogressbar.CircularProgressBar;
 
 /**
  * Created by Vipul Sharma on 3/30/2016.
@@ -87,9 +97,65 @@ public class ScrollingOrderBanners extends PagerAdapter {
     }
 
     private void openURL(String url) {
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setData(Uri.parse(url));
-        mContext.startActivity(i);
+        //Create a new dialog
+        final Dialog webViewDialog = new Dialog(mContext);
+        //Remove the dialog's title
+        webViewDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Inflate the contents of this dialog with the Views defined at 'webviewdialog.xml'
+        webViewDialog.setContentView(R.layout.webviewdialog);
+        //With this line, the dialog can be dismissed by pressing the back key
+        webViewDialog.setCancelable(true);
+
+        //Initialize the Button object with the data from the 'webviewdialog.xml' file
+        Button btClose = (Button) webViewDialog.findViewById(R.id.bt_close);
+        //Define what should happen when the close button is pressed.
+        btClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Dismiss the dialog
+                webViewDialog.dismiss();
+            }
+        });
+
+        final CircularProgressBar web_view_circular_progress_bar = (CircularProgressBar) webViewDialog.findViewById(R.id.web_view_circular_progress_bar);
+
+        //Initialize the WebView object with data from the 'webviewdialog.xml' file
+        final WebView webView = (WebView) webViewDialog.findViewById(R.id.wb_webview);
+        //Scroll bars should not be hidden
+        webView.setScrollbarFadingEnabled(false);
+        //Disable the horizontal scroll bar
+        webView.setHorizontalScrollBarEnabled(false);
+        //Enable JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
+        //Set the user agent
+        webView.getSettings().setUserAgentString("AndroidWebView");
+        //Clear the cache
+        webView.clearCache(true);
+        //Make the webview load the specified URL
+        webView.loadUrl(url);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                web_view_circular_progress_bar.setVisibility(View.GONE);
+                webView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                web_view_circular_progress_bar.setVisibility(View.GONE);
+                webView.setVisibility(View.VISIBLE);
+            }
+        });
+        webViewDialog.show();
     }
 
     @Override
